0f73b264c548cde790160a813850e8133a2abb22
Jerry Sims <jerrysims07@gmail.com>
2014-02-21 15:33:40 -0600

    Beginning to make dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #returns the proper title for each page.
+  def full_title(page_title)
+    base_title = "Draft Day"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
