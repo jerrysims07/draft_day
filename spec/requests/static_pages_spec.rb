b4b74e6b0b1f8b89df5de5cc1b07e2cb20916c5f
Jerry Sims <jerrysims07@gmail.com>
2014-02-24 16:06:38 -0600

    tests refactored somewhat

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,45 +3,41 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home Page" do
+    before { visit root_path}
     it "should have the content 'Draft Day'" do 
-      visit '/static_pages/home'
       expect(page).to have_content('Draft Day')
     end
     it "should have the right name" do
-      visit '/static_pages/home'
       expect(page).to have_title("Draft Day | Home")
     end 
   end
 
   describe "Help Page" do
+    before { visit help_path }
     it "should have the word 'Help'" do 
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     it "should have the right name" do
-      visit '/static_pages/help'
       expect(page).to have_title("Draft Day | Help")
     end 
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the word 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About')
     end
     it "should have the right name" do
-      visit '/static_pages/about'
       expect(page).to have_title("Draft Day | About")
     end 
   end
 
   describe "Contact page" do 
+    before { visit contact_path }
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact'" do 
-      visit '/static_pages/contact'
       expect(page).to have_title("Draft Day | Contact")
     end 
   end 
